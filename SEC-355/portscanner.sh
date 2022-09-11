036409f62d52074fbec77785115857a1f1d93371
Chromosom3 <36739730+Chromosom3@users.noreply.github.com>
2022-09-11 16:33:54 -0400

    parameter and option checking

--- SEC-355/portscanner.sh
+++ SEC-355/portscanner.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]
+then 
+    echo "Invalid amount of arguments supplied. You need to provide a hosts file and a ports file."
+    # General error exit
+    exit 1
+fi
+
 hostfile=$1
 portfile=$2
+
+if [ ! -f "$hostfile" ] | [ ! -f "$portfile" ]
+then 
+    echo "One or more supplied files not found. Exiting..."
+    exit 1
+fi
 
 echo "host,port"
 for host in $(cat $hostfile); do
