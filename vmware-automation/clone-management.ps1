92871b134f4b55a8a7963ac0db404f88df2d7beb
Chromosom3 <36739730+Chromosom3@users.noreply.github.com>
2022-10-25 11:36:35 -0400

    Restructured the code to be more modular.

--- vmware-automation/clone-management.ps1
+++ vmware-automation/clone-management.ps1
@@ -3,9 +3,7 @@
 # Description: Used to manage cloning of VMs in ESXi without vCenter Server
 # Needs PowerCLI Installed (Install-Module -Name VMware.PowerCLI)
 
-try {
-    Get-VMHost
-} catch {
+function Connect {
     # Connection Information
     $esxi_ip = Read-Host "Please enter the server IP address"
     $esxi_account = Get-Credential -Message "Please enter your ESXi Credentials"
@@ -24,8 +22,33 @@ try {
     Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $false -Confirm:$false
 }
 
-# Add code here for the rest of the script.
-
-if ($disconnect) {
+function Disconnect {
     Disconnect-VIServer -Server * -Force -Confirm:$false
+}
+
+function Menu {
+    try {
+        [uint16]$selection = Read-Host("[1] List VMs`n[2] Create Linked Clone`nSelection")
+    } catch {
+        Write-Host("Bad user... Invalid selection. Must be an integer.")
+    }
+    
+    switch ($selection)
+    {
+        1 {"It is one."}
+        2 {"It is two."}
+        20 {Disconnect}
+    }
+}
+
+
+try {
+    Get-VMHost
+} catch {
+    Connect
+} Finally {
+    Menu 
+    if ($disconnect) {
+        Disconnect 
+    }    
 }
