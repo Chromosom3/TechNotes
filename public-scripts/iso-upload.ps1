6eb62b424dfd717eb0cb4699211d05f0c2fcf355
Dylan N. <36739730+Chromosom3@users.noreply.github.com>
2025-01-16 09:05:57 -0800

    Create iso-upload.ps1
    Think this might work.

---
+++ public-scripts/iso-upload.ps1
@@ -0,0 +1,29 @@
+# Script: iso-upload.ps1
+# Author: Dylan 'Chromosom3' Navarro
+# Description: Used to make VMs in bulk
+# Needs PowerCLI Installed (Install-Module -Name VMware.PowerCLI)
+
+# Connection Information
+$vsphere_ip = Read-Host "Please enter the server IP address"
+$account = Get-Credential -Message "Please enter your vSphere Credentials"
+$content_lib_name = ""
+$iso_dir = ""
+
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
+Set-PowerCLIConfiguration -WebOperationTimeoutSeconds 3600 -Scope Session
+Connect-VIServer -Server $vsphere_ip -Credential $account
+
+$content_lib = Get-ContentLibrary -Name $content_lib_name
+
+if (!$content_lib) {
+    Write-Host "Content Library Not Found"
+    exit()
+}
+
+
+$isoFiles = Get-ChildItem -Path $directoryPath -Filter *.iso
+
+foreach ($isoFile in $isoFiles) {
+     Write-Host "Uploading $($isoFile)"
+    New-ContentLibraryItem -ContentLibrary $content_lib -Name $isoFile -Files $($isoFile.FullName)
+}
